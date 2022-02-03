69bd01d91dbd2110e5fe7a1a0021b6c1bfb5f996
manspitub <74324580+manspitub@users.noreply.github.com>
2022-02-03 10:10:43 +0100

    Comenzado el diseño de las pantallas

---
+++ meteor_app_spinola/lib/mars_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MarsScreen extends StatelessWidget {
+  const MarsScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+    
+  }
+}
